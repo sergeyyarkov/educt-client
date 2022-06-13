0568bad99d18b51cd735ce59a838f29e0990f74e
Sergey Yarkov <serzh.yarkov@gmail.com>
2022-06-13 03:20:44 +0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,6 @@ function renderChunks(deps: Record<string, string>) {
 // https://vitejs.dev/config/
 export default defineConfig({
   root: './',
-  base: '/educt-client/',
   publicDir: 'public',
   plugins: [react()],
   server: {
